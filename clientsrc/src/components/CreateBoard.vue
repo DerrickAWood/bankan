2d32e38d5d80cd22cf511940569ecc9237e82942
Brandon Reninger <brandonreninger@yahoo.com>
2020-04-24 15:24:12 -0600

    got rid of debugger and styled create board button

--- clientsrc/src/components/CreateBoard.vue
+++ clientsrc/src/components/CreateBoard.vue
@@ -3,31 +3,29 @@
     <form @submit.prevent="addBoard">
       <input type="text" placeholder="title" v-model="newBoard.title" required />
       <input type="text" placeholder="description" v-model="newBoard.description" />
-      <button type="submit">Create Board</button>
+      <button class="btn btn-sm btn-primary" type="submit">Create Board</button>
     </form>
-    
   </div>
 </template>
 
 <script>
-
 export default {
   name: "create-board",
-  data(){
-    return{
-    newBoard: {
+  data() {
+    return {
+      newBoard: {
         title: "",
         description: ""
       }
-    }
+    };
   },
   methods: {
     addBoard() {
       this.$store.dispatch("addBoard", this.newBoard);
       this.newBoard = { title: "", description: "" };
     }
-  },
   }
+};
 </script>
 
 <style></style>
