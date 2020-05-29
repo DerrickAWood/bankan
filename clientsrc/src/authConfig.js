43841ce9f50fae24ef4829fc9d4c44098e516ad0
Derrick A Wood <dwood_1994@yahoo.com>
2020-05-28 18:51:25 -0600

    auth

--- clientsrc/src/authConfig.js
+++ clientsrc/src/authConfig.js
@@ -1,3 +1,3 @@
 export const domain = "dev-4agk-0aw.auth0.com";
-export const audience = "kanban-project.com";
+export const audience = "TheKanBan.com";
 export const clientId = "8dDLECoapg2CbtBZ7rE37mwsAXS0FrvI";
